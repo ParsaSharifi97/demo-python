4c31667b590b2694afa9bd39a90214334cd1c48c
ParsaS. <75152486+ParsaSharifi97@users.noreply.github.com>
2025-05-23 20:05:50 -0400

    Create test.py
    sad

---
+++ test.py
@@ -0,0 +1,4 @@
+# WARNING: This file contains a hardcoded secret and should be flagged by security tools.
+
+API_KEY = "sk-live-51c6e7b0ab1234567890abcdef123456"
+DATABASE_PASSWORD = "SuperSecretPassword123!"
